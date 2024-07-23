c1b6a3fc258fd3790498f654e1ec9342d83c17ff
CabbageInc <memo4eb@gmail.com>
2024-07-23 15:31:23 -0500

    Added new example

---
+++ chapter5example1/Chapter5ex1.c
@@ -0,0 +1,12 @@
+// Example program #1 from Chapter 5 of Absolute Beginner's Guide to C, 3rd Edition
+//File Chapter5ex1.c
+
+/* This is a sample program that lists three kids and their school supply needs, as well as cost to buy the supplies */
+
+#include <stdio.h>
+
+int main(){
+    printf("test output");
+    
+    return 0;
+}
